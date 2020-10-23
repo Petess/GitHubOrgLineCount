5e4d1ccaac7de2e1823bbe83e9598f77703309ae
Peter S <peter.sienkowski@ga.gov.au>
2020-10-23 16:49:48 +1100

    Initial check in to get the repositories

---
+++ GetOrgRepos.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from github import Github
+
+def getCredentials():
+    f = open('.creds')
+    creds = f.readline()
+    f.close()
+    return creds
+
+def GetRepos():
+    g = Github( getCredentials() ) 
+    count = 0
+    for repo in g.get_organization("GeoscienceAustralia").get_repos():
+        print( repo.name )
+        count = count + 1
+    print( "There are " + str( count ) + " repositories" )
+
+def themain():
+    print("Started")
+    GetRepos()
+    
+if __name__ == "__main__":
+    themain()
