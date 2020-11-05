e63283a587f382348dbac3c86a6e5df9dc411e72
Peter S <peter.sienkowski@ga.gov.au>
2020-11-06 10:48:49 +1100

    Change the script to remove forks of other repos

--- GetOrgRepos.py
+++ GetOrgRepos.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from github import Github
+import argparse
 
 def getCredentials():
     f = open('.creds')
@@ -8,16 +9,33 @@ def getCredentials():
     f.close()
     return creds
 
-def GetRepos():
+def GetReposOfType(theType):
     g = Github( getCredentials() ) 
-    count = 0
-    for repo in g.get_organization("GeoscienceAustralia").get_repos(type = 'public' ):
-        print( repo.clone_url ) 
-        count = count + 1
-#    print( "There are " + str( count ) + " repositories" )
+    returnList = []
+
+    for repo in g.get_organization("GeoscienceAustralia").get_repos(type = theType ):
+        returnList.append( repo.clone_url )
+
+    return returnList
+
+def writeFile( fileName, ListToWriteOut ):
+    outfile = open(fileName,'w')
+    for item in ListToWriteOut:
+        outfile.write( item + "\n")
+    outfile.close()
+    return
 
 def themain():
-    GetRepos()
-    
+    parser = argparse.ArgumentParser()
+    requiredName = parser.add_argument_group('required named arguments')
+    parser.add_argument('-f', '--file',
+                        help='Filename to write out', required=True)
+    args = parser.parse_args()
+
+    forkRepoList = GetReposOfType('forks')
+    publicRepoList = GetReposOfType('public')
+    publicWithoutForks = [x for x in publicRepoList if x not in forkRepoList]
+    writeFile( args.file, publicWithoutForks)     
+
 if __name__ == "__main__":
     themain()
