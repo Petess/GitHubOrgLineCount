ebbf48a0584502532d4da7d5939594658366b42b
EC2 Default User <ec2-user@ip-172-31-17-68.ap-southeast-2.compute.internal>
2020-11-19 22:56:56 +0000

    Changed system to download one repo at a time, then get stats, then sum the stats

--- repoListLineCount.sh
+++ repoListLineCount.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 filename="$1"
+projectTotalsDir="$2" 
 
 all_lines=`cat $filename`
+
+if (( $# != 2 )); then
+    >&2 echo "Required parameters - filename resultsDirectory"
+fi
+
+mkdir temp-repo
+mkdir $projectTotalsDirOA
 
 for item in $all_lines;
 do
@@ -11,8 +19,14 @@ do
     projectName2=${projectName##*/}
     
     git clone --depth 1 "$item" temp-repo/$projectName2 &&
-    cloc --out temp-project-totals/$projectName2.txt temp-repo/$projectName2 &&
+    cloc --out $projectTotalsDir/$projectName2.txt temp-repo/$projectName2 &&
     rm -rf temp-repo/$projectName2 
 
     echo $item
 done
+
+rmdir temp-repo
+
+cloc --csv --sum_reports -out Summary_Results $projectTotalsDir/*.txt
+
+
